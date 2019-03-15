5a013366d3749a9f3ef31f34beb41d9968f816c8
Frost <ymiku@qq.com>
2019-03-15 09:58:20 +0800

    xml

--- Assets/Save Manager/XMLSaver.cs
+++ Assets/Save Manager/XMLSaver.cs
@@ -8,51 +8,24 @@ public class XMLSaver : MonoBehaviour {
 	// Use this for initialization
 	SaveData s;
 	SaveData b;
-	public void Save () {
+	public void Save (SaveData data) {
 		XmlSerializer serializer = new XmlSerializer (typeof(SaveData));
 		FileStream stream = new FileStream (Application.persistentDataPath + "/save.xml", FileMode.Create);
-		serializer.Serialize (stream, s);
+		serializer.Serialize (stream, data);
 		stream.Close ();
 	}
 
 
-	public void Load () {
+	public SaveData Load () {
+		SaveData data = null;
 		if (File.Exists (Application.dataPath + "/save.xml")) {
 			XmlSerializer serializer = new XmlSerializer (typeof(SaveData));
 			FileStream stream = new FileStream (Application.persistentDataPath + "/save.xml", FileMode.Open);
-			b = serializer.Deserialize (stream) as SaveData;
+			data = serializer.Deserialize (stream) as SaveData;
 			stream.Close ();
 		} else {
 			Debug.LogError ("Save Something First");
 		}
+		return data;
 	}
-	void Start () {
-		s = new SaveData ();
-		s.aa = new List<MyClass2> ();
-		s.aa.Add (new MyClass2());
-		s.aa[0].a = 1;
-		s.aa[0].ss = "asdsdsd";
-
-		Save ();
-		Load ();
-	
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
-[System.Serializable]
-public class SaveData
-{
-	
-	public List<MyClass2> aa;
-}
-[System.Serializable]
-public class MyClass2
-{
-	public int a;
-	public int b;
-	public string ss;
 }
